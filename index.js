3ff2b62b86d7fe7e1de4b4604cba36be071279bf
Suman Chakraborty <sumanchakra8145@gmail.com>
2021-03-11 01:45:25 +0530

    landing page and signup page almost done

--- index.js
+++ index.js
@@ -23,3 +23,18 @@ function lightbox_close() {
     document.getElementById('fade').style.display = 'none';
     lightBoxVideo.pause();
 }
+
+
+// window.onscroll=function(){
+//     console.log(scrollY)
+// var div= document.querySelector('#caseNav')
+// div.classList.add('styckynav');
+// var imagnav= document.querySelector('#logo')
+// imagnav.classList.add("logo1")
+// if(scrollY<=50){
+//     div.classList.remove('styckynav');
+//     imagnav.classList.remove("logo1")
+// }
+// }
+
+document.querySelector("")
