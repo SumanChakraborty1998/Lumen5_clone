4cea4045e985945ec14a6ad9e182f7ef320a7324
Suman Chakraborty <sumanchakra8145@gmail.com>
2021-03-11 17:33:12 +0530

    landing page, login page and sign up page full done

--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,24 @@
 
 
+
+
+var video = document.querySelectorAll("video");
+  
+video.forEach((video) => {
+    video.addEventListener("mouseover", function () {
+    this.play();
+    });
+    video.addEventListener("mouseout", function () {
+    this.pause();
+    });
+});
+
+function toggel() {
+    video.classList.toggel("active");
+    video.pause();
+    video.currentTime = 0;
+}
+
 window.document.onkeydown = function (e) {
     if (!e) {
         e = event;
@@ -25,17 +44,6 @@ function lightbox_close() {
 }
 
 
-// window.onscroll=function(){
-//     console.log(scrollY)
-// var div= document.querySelector('#caseNav')
-// div.classList.add('styckynav');
-// var imagnav= document.querySelector('#logo')
-// imagnav.classList.add("logo1")
-// if(scrollY<=50){
-//     div.classList.remove('styckynav');
-//     imagnav.classList.remove("logo1")
-// }
-// }
 
 document.querySelector("")
 
@@ -46,67 +54,34 @@ function redirectMeToSignUp()
 
 
 
-// Mangesh Part 5********************
 
 
-
-//   var figure = $(".video-block-video").hover(hoverVideo, hideVideo);
-
-//   function hoverVideo(e) {
-//     $("video", this).get(0).play();
-//   }
-
-//   function hideVideo(e) {
-//     $("video", this).get(0).pause();
-//   }
-
-//   video.on("mouseover", function () {
-//     video.get(0).play();
-//   });
-
-//   video.on("mouseout", function () {
-//     video.get(0);
-//   });
-
-//   function playVideo() {
-//     video.play();
-//   }
-//   function pauseVideo() {
-//     video.pause();
-//   }
-
-//   video.addEventListener("mouseover", playVideo);
-//   video.addEventListener("mouseout", pauseVideo);
+// mangesh 6 th part
 
 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("class2");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  console.log(x, n);
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "flex";
+}
 
 
+//mangesh 6 part end
 
-
-
-
-
-
-
-
-
-// var video = document.querySelectorAll("video");
-
-// video.forEach((video) => {
-// video.addEventListener("mouseover", function () {
-//     this.play();
-// });
-// video.addEventListener("mouseout", function () {
-//     this.pause();
-// });
-
-// });
-
-// function toggel() {
-// video.classList.toggel("active");
-// video.pause();
-// video.currentTime = 0;
-// }
-
-
-// Mangesh Part 5******************** END
